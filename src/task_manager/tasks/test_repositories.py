ae62721032723d37d704258cb106ff2c7b3d6b9a
brunodantas <bruno.welldm@gmail.com>
2025-10-13 16:12:24 -0300

    test queryset

--- src/task_manager/tasks/test_repositories.py
+++ src/task_manager/tasks/test_repositories.py
@@ -1,9 +1,24 @@
+import pytest
 from tasks.models import Task
-from tasks.repositories import DummyTaskRepository
+from tasks.repositories import DummyTaskRepository, TaskRepository
 
 
-def test_repository():
+@pytest.mark.django_db
+def test_dummy_repository():
     repo = DummyTaskRepository()
+    repo.insert(Task(id=1, title="Task 1", description="Desc"))
+    assert len(repo) == 1
+    assert 1 in repo
+    task = repo[1]
+    assert task.title == "Task 1"
+    assert task.description == "Desc"
+    assert task.cost == 1
+    assert list(repo)[0].id == 1
+
+
+@pytest.mark.django_db
+def test_queryset_repository():
+    repo = TaskRepository()
     repo.insert(Task(id=1, title="Task 1", description="Desc"))
     assert len(repo) == 1
     assert 1 in repo
